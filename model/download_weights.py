8757f46eb786944cda61f68fc67d33f05bac28f5
phy_ml <abhi.upes01@outlook.com>
2022-12-09 20:42:35 +0800

    add model download script

---
+++ model/download_weights.py
@@ -0,0 +1,15 @@
+# command to download weights
+import os
+import urllib.request
+
+def download():
+    if not os.path.exists('model_weight'):
+        os.makedirs('model_weight')
+
+    url = "https://download.pytorch.org/models/vgg19-dcbb9e9d.pth"
+
+    urllib.request.urlretrieve(url,"model_weight/vgg19-dcbb9e9d.pth")
+    print('Download finished')
+
+if __name__ == "__main__":
+    download()
